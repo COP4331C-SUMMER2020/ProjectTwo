c52ce80be2b04ff79196160c089bb1fb943f2d6a
Devon <danselmo42@outlook.com>
2020-06-23 16:37:11 -0400

    Recipe scraper now runs through all recipes on main page and prints name, ingredients, and procedure. Issue with too many fetches all at once perhaps, but added a 3 second wait and works most of the time now. Several recipes will still have to be fixed by hand.

--- backEnd/scraper.js
+++ backEnd/scraper.js
@@ -2,11 +2,14 @@ const jsdom = require("jsdom");
 const fetch = require('isomorphic-fetch')
 const { JSDOM } = jsdom;
 const baseURL = "https://en.wikibooks.org/wiki/Cookbook:";
-var recipesNames = [];
+var recipeNames = [];
+var recipeIngredients = [];
+var recipeProcedures = [];
+var badIndeces = [563];
+badIndeces.fill(0)
 var index = 0;
 recipeNamerIDKwhoKnows();
 
-//TO-DO: need to deal with recipes who's names change slightly in url vs main page, as well as ones with multiple words (need '_')
 
 //function will go through each recipe on main page, send each recipe name to recipeScraper to open
 async function recipeNamerIDKwhoKnows()
@@ -24,11 +27,15 @@ async function recipeNamerIDKwhoKnows()
 		name.textContent = name.textContent.replace("(pt)","");
 		if (index > 562)
 			break;
+		name.textContent = name.textContent.split("(")[0];
 		console.log(name.textContent)
+		
 		await recipeScraper(name.textContent);
+		await doSomething();
 		console.log("\n");
 		index++;
-	}		
+	}
+	
 }
 
 //will take url/name and log the ingredients and procedure of that recipe
@@ -37,14 +44,35 @@ async function recipeScraper(recipe){
 	const response = await fetch(result);
 	const text = await response.text();
 	const dom = await new JSDOM(text);
-	if (dom.window.document.getElementById("Ingredients").parentNode.nextElementSibling.className == "thumb tright")
-		console.log(dom.window.document.getElementById("Ingredients").parentNode.nextElementSibling.nextElementSibling.textContent);
-	else
-		console.log(dom.window.document.getElementById("Ingredients").parentNode.nextElementSibling.textContent);
+	try
+	{
+		var temp = dom.window.document.getElementById("Ingredients").parentNode.nextElementSibling;
+		while (temp.className == "thumb tright")
+		{
+			temp = temp.nextElementSibling;
+		}
+		console.log(temp.textContent);
+		
+		
+		var temp2 = dom.window.document.querySelector('[id^=Procedure]').parentNode.nextElementSibling;
+		while (temp2.className == "thumb tright")
+		{
+			temp2 = temp2.nextElementSibling;
+		}
+		console.log(temp2.textContent);
 
-	if (dom.window.document.querySelector('[id^=Procedure]').parentNode.nextElementSibling.className == "thumb tright")
-		console.log(dom.window.document.querySelector('[id^=Procedure]').parentNode.nextElementSibling.nextElementSibling.textContent);
-	else
-		console.log(dom.window.document.querySelector('[id^=Procedure]').parentNode.nextElementSibling.textContent);
-	return;
+	}
+	catch (err)
+	{
+		console.log(err);
+	}
+	//return;
+}
+
+const doSomething = async () => {
+  await sleep(3000)
+}
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
 }

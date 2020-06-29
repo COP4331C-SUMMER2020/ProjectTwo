717698ad1ca5e5c7372eda76a7e9460d555f04da
Devon <danselmo42@outlook.com>
2020-06-29 13:45:40 -0400

    Added a testIndex.js file that was originally for testing how to search with a text index. Was rendered moot once wildcard searched were found and the index was removed. The file now shows foundation for how to search and return appropriate fields.

---
+++ backEnd/testIndex.js
@@ -0,0 +1,17 @@
+var url = "mongodb+srv://group24:elevenbrethren@group24-ityll.mongodb.net/group24?retryWrites=true&w=majority";
+ 
+// create a client to mongodb
+var MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(url, function(err, db) {
+	if (err) throw err;
+	var dbo = db.db("24-7_Recipes");
+	//var query = {  $text: { $search: "\"vanilla\"" }  };
+	// " .* " means 0 or more characters, so pad the search with /..../, then use .* as wildcard
+	// projection is which fields to show, default is all
+	dbo.collection("Recipe Docs").find({Ingredients: /.*vanilla.*/}, { projection: { _id: 1, Recipe: 1} }).toArray(function(err, result) {
+		if (err) throw err;
+		console.log(result);
+		db.close();
+	});
+});

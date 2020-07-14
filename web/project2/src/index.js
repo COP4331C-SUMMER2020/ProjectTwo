0f346bdd9aca2fc222f42e7c16c936d221d249ff
Zachary Sutrich <abocreature@gmail.com>
2020-07-14 17:59:26 -0400

    created barebones index page

--- web/project2/src/index.js
+++ web/project2/src/index.js
@@ -11,6 +11,187 @@ const INITIAL_STATE = {
   password: ""
 };
 
+function Index() {
+	const {
+		handleSubmit,
+		handleChange,
+		handleBlur,
+		values,
+		errors,
+		isSubmitting
+	} = useFormValidation(INITIAL_STATE, validateAuth);
+	
+	return (
+		<div>
+			<div>
+				<Login />
+				<Register />
+			</div>
+			<div>
+				<IngredientSearch />
+				<RecipeSearch />
+				<AddRecipe />
+			</div>	
+			<div>
+				<APIreturn />
+			</div>
+		</div>
+	);
+}
+
+function APIreturn() {
+	return (
+		<div className="container">
+			<h1>API Return</h1>
+			<textarea
+				id="apireturn"
+				name="apireturn"
+				row="4"
+				cols="50"
+			/>
+		</div>
+	);
+}
+
+function AddRecipe() {
+	const {
+		handleSubmit,
+		handleChange,
+		handleBlur,
+		values,
+		errors,
+		isSubmitting
+	} = useFormValidation(INITIAL_STATE, validateAuth);
+	
+	return (
+		<div className="container">
+			<h1>Add Recipe</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					onChange={handleChange}
+					onBlur={handleBlur}
+					name="newrecipename"
+					autoComplete="off"
+					placeholder="Recipe Name"
+				/>
+				<div>
+				<button disabled={isSubmitting} type="submit">
+					Submit
+				</button>
+				</div>
+			</form>
+		</div>
+	);
+}
+
+function IngredientSearch() {
+	const {
+		handleSubmit,
+		handleChange,
+		handleBlur,
+		values,
+		errors,
+		isSubmitting
+	} = useFormValidation(INITIAL_STATE, validateAuth);
+	
+	return (
+		<div className="container">
+			<h1>Ingredient Search</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					onChange={handleChange}
+					onBlur={handleBlur}
+					name="ingredientname"
+					autoComplete="off"
+					placeholder="Search Term"
+				/>
+				<div>
+				<button disabled={isSubmitting} type="submit">
+					Submit
+				</button>
+				</div>
+			</form>
+		</div>
+	);
+}
+
+function RecipeSearch() {
+	const {
+		handleSubmit,
+		handleChange,
+		handleBlur,
+		values,
+		errors,
+		isSubmitting
+	} = useFormValidation(INITIAL_STATE, validateAuth);
+	
+	return (
+		<div className="container">
+			<h1>Recipe Search</h1>
+			<form onSubmit={handleSubmit}>
+				<input
+					onChange={handleChange}
+					onBlur={handleBlur}
+					name="recipename"
+					autoComplete="off"
+					placeholder="Search Term"
+				/>
+				<div>
+				<button disabled={isSubmitting} type="submit">
+					Submit
+				</button>
+				</div>
+			</form>
+		</div>
+	);
+}
+
+function Login() {
+  const {
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    values,
+    errors,
+    isSubmitting
+  } = useFormValidation(INITIAL_STATE, validateAuth);
+  // const [email, setEmail] = React.useState("");
+  // const [password, setPassword] = React.useState("");
+
+  return (
+    <div className="container">
+      <h1>Login Test</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          name="loginemail"
+          value={values.email}
+          className={errors.email && "error-input"}
+          autoComplete="off"
+          placeholder="Your email address"
+        />
+        {errors.email && <p className="error-text">{errors.email}</p>}
+        <input
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.password}
+          className={errors.password && "error-input"}
+          name="loginpassword"
+          type="password"
+          placeholder="Choose a safe password"
+        />
+        {errors.password && <p className="error-text">{errors.password}</p>}
+        <div>
+          <button disabled={isSubmitting} type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
 function Register() {
   const {
     handleSubmit,
@@ -25,12 +206,12 @@ function Register() {
 
   return (
     <div className="container">
-      <h1>Register Here</h1>
+      <h1>Register Test</h1>
       <form onSubmit={handleSubmit}>
         <input
           onChange={handleChange}
           onBlur={handleBlur}
-          name="email"
+          name="registeremail"
           value={values.email}
           className={errors.email && "error-input"}
           autoComplete="off"
@@ -42,7 +223,7 @@ function Register() {
           onBlur={handleBlur}
           value={values.password}
           className={errors.password && "error-input"}
-          name="password"
+          name="registerpassword"
           type="password"
           placeholder="Choose a safe password"
         />
@@ -58,4 +239,4 @@ function Register() {
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Register />, rootElement);
+ReactDOM.render(<Index />, rootElement);

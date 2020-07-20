4a895e71b43a140a8b8072b918be4795b49403e9
elmiki95 <65347855+elmiki95@users.noreply.github.com>
2020-07-19 20:27:14 -0400

    Add files via upload

--- Front-End_App/src/screens/DrawerContainer/DrawerContainer.js
+++ Front-End_App/src/screens/DrawerContainer/DrawerContainer.js
@@ -1,1 +1,48 @@
+import React from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import styles from './styles';
+import MenuButton from '../../components/MenuButton/MenuButton';
+import { Drawer } from 'native-base';
 
+export default class DrawerContainer extends React.Component {
+  render() {
+    const { navigation } = this.props;
+    return (
+      <View style={styles.content}>
+        <View style={styles.container}>
+          <MenuButton
+            title="HOME"
+            source={require('../../../assets/icons/home.png')}
+            onPress={() => {
+              navigation.navigate('Home');
+              navigation.closeDrawer();
+            }}
+          />
+          <MenuButton
+            title="SEARCH"
+            source={require('../../../assets/icons/search.png')}
+            onPress={() => {
+              navigation.navigate('Search');
+              navigation.closeDrawer();
+            }}
+          />
+          <MenuButton
+            title="ADD RECIPE"
+            source={require('../../../assets/icons/addRecipe.png')}
+            onPress={() => {
+              navigation.navigate('Add Recipe');
+              navigation.closeDrawer();
+            }}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+DrawerContainer.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired
+  })
+};

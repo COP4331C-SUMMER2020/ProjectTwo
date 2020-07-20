b44a32d5c5b0e392f0d2f6a1800664fc8715e823
elmiki95 <65347855+elmiki95@users.noreply.github.com>
2020-07-19 20:23:04 -0400

    Add files via upload

--- Front-End_App/src/navigations/AppNavigation.js
+++ Front-End_App/src/navigations/AppNavigation.js
@@ -1,1 +1,52 @@
+import { createAppContainer } from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer'
+import {createStackNavigator} from 'react-navigation-stack'
+import HomeScreen from '../screens/Home/HomeScreen';
+import CategoriesScreen from '../screens/Categories/CategoriesScreen';
+import RecipeScreen from '../screens/Recipe/RecipeScreen';
+import RecipesListScreen from '../screens/RecipesList/RecipesListScreen';
+import DrawerContainer from '../screens/DrawerContainer/DrawerContainer';
+import IngredientScreen from '../screens/Ingredient/IngredientScreen';
+import SearchScreen from '../screens/Search/SearchScreen';
+import IngredientsDetailsScreen from '../screens/IngredientsDetails/IngredientsDetailsScreen';
+import AddRecipe from '../screens/AddRecipe/AddRecipeScreen';
 
+const MainNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Categories: CategoriesScreen,
+    Recipe: RecipeScreen,
+    RecipesList: RecipesListScreen,
+    Ingredient: IngredientScreen,
+    Search: SearchScreen,
+    IngredientsDetails: IngredientsDetailsScreen
+  },
+  {
+    initialRouteName: 'Home',
+    
+    defaultNavigationOptions: ({ navigation }) => ({
+      headerTitleStyle: {
+        flex: 1,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        alignSelf: 'center',
+      }
+    })
+  }
+); 
+
+const DrawerStack = createDrawerNavigator(
+  {
+    Main: MainNavigator
+  },
+  {
+    drawerPosition: 'left',
+    initialRouteName: 'Main',
+    drawerWidth: 190,
+    contentComponent: DrawerContainer
+  }
+);
+ 
+export default AppContainer = createAppContainer(DrawerStack);
+
+console.disableYellowBox = true;
